18156407cd01c6074b618922c44dffc4ebaee9a7
nikhilkawdeti28@gmail.com <nikhilkawdeti28@gmail.com>
2023-10-25 17:51:16 +0530

    25 oct evening

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from './components/day4components';
+// import Main from './components/day 4/day4components';
+import D4l2ex1 from './components/day 4/day4level2exercise1';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Main /> */}
+      <D4l2ex1 />
     </div>
   );
 }
---
+++ src/components/day 4/day4level2exercise1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function container() {
+    <div className=""></div>
+}
+
+export default function d4l2ex1() {
+  const h3Center = {
+    textAlign: "center",
+  };
+
+  return (
+    <>
+      <h1>hello from d4 l2 ex1</h1>
+      <h3 style={h3Center}>Front End Tech</h3>
+      <container />
+    </>
+  );
+}
