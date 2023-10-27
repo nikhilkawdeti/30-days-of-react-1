fdbefc8aff6f9dbd07b55eab66ff932a0f9de1ac
nikhilkawdeti28@gmail.com <nikhilkawdeti28@gmail.com>
2023-10-28 00:01:19 +0530

    27 oct night

--- src/components/day 4/day4level2exercise1.jsx
+++ src/components/day 4/day4level2exercise1.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import tailwind from '../../index.css'
+import htmlLogo from "../../assets/html-logo.jpg";
+import cssLogo from "../../assets/css-logo.png";
+import jsLogo from "../../assets/js-logo.png";
+import reactLogo from "../../assets/react-logo.png";
 
 export function Container() {
-    return(
-      <><div className="">container`1</div></>
-    )
+  return (
+    <>
+      <div className="">
+        <ul
+          className="flex flex-row"
+          style={{ listStyle: "none" }}
+        >
+          <li className="">
+            <img src={htmlLogo} alt="" srcset="" width={100} height={100} />
+          </li>
+          <li className="">
+            <img src={cssLogo} alt="" srcset="" width={100} height={100} />
+          </li>
+          <li className="">
+            <img src={jsLogo} alt="" srcset="" width={100} height={100} />
+          </li>
+          <li className="">
+            <img src={reactLogo} alt="" srcset="" width={100} height={100} />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default function D4l2ex1() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     colors: {
       blue: "#1fb6ff",
