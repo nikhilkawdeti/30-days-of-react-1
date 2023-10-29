5422669c4830f1a2cf9ac2e88b727a3074a63568
nikhilkawdeti28@gmail.com <nikhilkawdeti28@gmail.com>
2023-10-30 00:23:32 +0530

    29 night d4 l2 ex3 complete

--- src/components/day 4/day4level2exercise3.jsx
+++ src/components/day 4/day4level2exercise3.jsx
@@ -1,31 +1,86 @@
 import React from "react";
 
 const MainContainer = () => {
-  <>mc</>;
+  return (
+    <div className={"container mx-auto my-2 p-4 mt-5 rounded-md bg-[#a4b5e6]"}>
+      <SecondContainer />
+    </div>
+  );
 };
 
 const SecondContainer = () => {
-  <div className="">sc</div>;
+  return (
+    <>
+      <div className="container mx-auto rounded-md bg-gray-500">
+        <Heading />
+      </div>
+    </>
+  );
 };
 
 const Heading = () => {
-  <p>head</p>;
+  return (
+    <>
+      <div className="flex justify-center">
+        <h3 className="text-3xl">SUBSCRIBE</h3>
+      </div>
+      <div className=" flex justify-center">
+        <h4 className="text-[15px]">
+          Sign Up With Your Email Address To Receive News And Updates
+        </h4>
+      </div>
+      <Input />
+    </>
+  );
 };
 
-const Input = <div className="">inp</div>;
+function Input() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="mx-4 my-3">
+          <input
+            type="text"
+            className="rounded px-2 py-2"
+            placeholder="First Name"
+          />
+        </div>
+        <div className="mx-4 my-3">
+          <input
+            type="text"
+            className="rounded px-2 py-2"
+            placeholder="User Name"
+          />
+        </div>
+        <div className="mx-4 my-3">
+          <input
+            type="email"
+            className="rounded px-2 py-2"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+      <SubscribeButton />
+    </>
+  );
+}
 
 function SubscribeButton() {
-  <>sbu</>;
+  return (
+    <>
+      <div className="flex justify-center">
+        <button className="btn bg-red-600 rounded-md my-2 px-10 py-1 text-2xl">
+          Subscribe
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default function D4l2ex3() {
   return (
     <>
       <MainContainer />
-      <SecondContainer />
-      <Heading />
-      <Input />
-      <SubscribeButton />
     </>
   );
 }
